2b88f15601a5578d05ee2a4d794054f6ed91351c
Georgina5-2 <122113060+Georgina5-2@users.noreply.github.com>
2023-03-25 16:09:18 -0400

    previously searched cities caching and bootstrap changes

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
-var geoCodingEndPoint="http://api.openweathermap.org/geo/1.0/";
+var geoCodingURL="http://api.openweathermap.org/geo/1.0/";
 var weatherReportAPIKey="9ad4c63b363b3077fb52384a9c5eb16c";
-// var cityName, stateCode, countryCode;
-// var weatherForecastEndPoint="http://api.openweathermap.org/data/2.5/forecast/lat=";
-function geoCoding(latCoords,lonCoords)
+var weatherForecastEndPoint="http://api.openweathermap.org/data/2.5/forecast?lat=";
+var displayCitiesArray=[];
+
+// searchedCity=[];
+// displaySearchedCity=[];
+
+async function generateWeatherForecast(latCoords, lonCoords)
 {
-
+    var weatherParametersArray=[];
+    weatherConditions=weatherForecastEndPoint+latCoords+"&lon="+lonCoords+"&appid="+weatherReportAPIKey;
+    var response=await fetch(weatherConditions);
+    var weatherConditionsResponse= await response.json();
+    console.log('weatherConditionsResponse ::', weatherConditionsResponse);
+    
 }
+        
 
-
-// var weatherReport=$('#weatherReportTxt');
 async function getGeoLocation()
 {
-    
+    // event.PreventDefault();
     var readCityName=$('#readCity').val();
     console.log(readCityName);
     var cityDetails=[];
@@ -19,57 +27,105 @@ async function getGeoLocation()
     console.log(cityDetails);
     console.log(typeof cityDetails);
     var cityName=cityDetails[0];
-    var cityLocation=cityDetails[1].trim();
-    if(!cityLocation.length===2)
+    console.log("CityName:",cityName);
+    previouslyStoredCity=localStorage.getItem("previouslySearchedCity");
+    console.log("local storage data:",previouslyStoredCity);
+    if(!previouslyStoredCity)
     {
-        alert("Enter the city location in abbreviations");
+        localStorage.setItem("previouslySearchedCity",cityName);
     }
     else
     {
-        if(cityLocation="")
-        {
-            var geoCodingParams=geoCodingEndPoint+"direct?q="+cityName+"&limit=5&appid="+weatherReportAPIKey;
-        }
-        else
-        {
-            var geoCodingParams=geoCodingEndPoint+"direct?q="+cityName+","+cityLocation+"&limit=5&appid="+weatherReportAPIKey;
-        }
+        localStorage.setItem("previouslySearchedCity",previouslyStoredCity+","+cityName);
+        
+        
     }
+
     
-    // // var storeCityDetails=localStorage.setItem("cityDetails")
-    // var geoCodingParams= "http://geoCodingEndPoint+'direct?q="+readCityName+"&limit=5&appid=9ad4c63b363b3077fb52384a9c5eb16c";
-    var response= await fetch(geoCodingParams);
-    var geoCodingResponse= await response.json();
-    var coordsArray=[];
-    for(i=0;i<5;i++)
+    
+    var cityLocation='';
+
+    if (cityDetails && cityDetails.length > 1) 
     {
-        var latCoords=geoCodingResponse[i].lat;
-        var lonCoords=geoCodingResponse[i].lon;
-        var country=geoCodingResponse[i].country;
-        var state=geoCodingResponse[i].state;
-        if(cityLocation="")
+        cityLocation = ','+cityDetails[1].trim().toUpperCase();
+        console.log("city location",cityLocation);
+        if (cityLocation.length!==3)
         {
-            geoCoding(latCoords,lonCoords);
+            alert("Enter the city location in abbreviations");
         }
-        else
+    } 
+    
+    var geoCodingEndpoint=geoCodingURL+"direct?q="+cityName+cityLocation+"&limit=1&appid="+weatherReportAPIKey;    
+    
+    var response= await fetch(geoCodingEndpoint);
+    var geoCodingResponse= await response.json();
+    console.log('geoCodingResponse :: ', geoCodingResponse);
+
+
+        var latCoords=geoCodingResponse[0].lat;
+        console.log("latitude:",latCoords);
+        var lonCoords=geoCodingResponse[0].lon;
+        console.log("longitude:",lonCoords);
+
+        if(geoCodingResponse && geoCodingResponse.length > 0)
         {
-            if(Lowercase(state)==lowercase(cityLocation)||lowercase(country)==lowercase(cityLocation))
-            {
-                preferredLatCoords=latCoords;
-                preferredLonCoords=lonCoords;
-                geoCoding(preferredLatCoords,preferredLonCoords);
-            }
-            else
-            {
-                var coordsCollection={"latitude":latCoords,"longitude":lonCoords,"country":country,"state":state};
-                coordsArray.push(coordsCollection);
-            }
-            
+            generateWeatherForecast(latCoords,lonCoords);
+        } 
+        else 
+        {
+            alert('No Matching location found');
+            return;
         }
+                
+}
+// function displayCurrentWeatherCondition()
+// {
+
+// }
+ function displayPreviouslySearchedCities()
+{
+    let displaySearchedCity=localStorage.getItem("previouslySearchedCity");
+    if(displaySearchedCity && !(displaySearchedCity.includes(',')))
+    { 
+        let searchedCity=$("<p>").addClass("btn btn-secondary btn-sm mb-3 mx-3 text-dark font-weight-bold").text(displaySearchedCity).attr("style","width:300px");
+        $("#searchedCities").append((searchedCity),'<br/>');
+        // var searchedCityText=$("#searchedCities").children().innerHTML;
+        // console.log("Name of the city:", searchedCityText);
+        // searchedCity.on("click",displayCurrentWeatherCondition());
+
+       
     }
-    console.log("list of coords:",coordsArray);
+    else
+    {
+        if(displaySearchedCity)
+        {
+            var storedCities=localStorage.getItem("previouslySearchedCity");
+            citiesArray=storedCities.split(',');
+            console.log(citiesArray);
+            for(let j=0;j<citiesArray.length;j++)
+            {
+                let searchedCity=$("<p>").addClass("btn btn-secondary btn-sm mb-3 mx-3 text-dark font-weight-bold").text(citiesArray[j]).attr("style","width:300px");
+                $("#searchedCities").append((searchedCity),'<br/>');
+                // var searchedCityText=$("#searchedCities").children().innerHTML;
+                // console.log("Name of the city:", searchedCityText);
+                // searchedCity.on("click",displayCurrentWeatherCondition());
+            }
+        }
+        
+    }
+    // let citiesArray= displayCitiesArray.push(displaySearchedCity);
+    // for(let i=0;i<citiesArray.length;i++)
+    // {
+    //     $("#searchedCities").append($("<p>").addClass("btn btn-secondary btn-sm mb-3").text(displaySearchedCity).attr("style","width:300px"));
+    // }
     
 }
-
-var btnSubmitCity=$('#submitCity');
-btnSubmitCity.on('click', getGeoLocation);
+function init()
+{
+    var citiesArray=[];
+    displayPreviouslySearchedCities();
+    var btnSubmitCity=$('#submitCity');
+    btnSubmitCity.on('click', getGeoLocation);
+    
+}
+init();
